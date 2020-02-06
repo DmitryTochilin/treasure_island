bf4fc41fdcb80a4bda9a85b5d7df28285685e300
DmitryTochilin <Dmitry.Tochilin@firstlinesoftware.com>
2020-02-06 11:07:52 +0300

    Some refactoring in Program class

--- ObjectsOnMap.cs
+++ ObjectsOnMap.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static TreasureIsland.Helper;
+using static TreasureIsland.Program;
 
 namespace TreasureIsland
 {
@@ -14,22 +16,22 @@ namespace TreasureIsland
                 while (x1 >= x2)
                 {
                     y2 = defaultY;
-                    Program.Frame[y2][x2 += 1] = Program.Home;
-                    Helper.Render();
+                    Frame[y2][x2 += 1] = Home;
+                    Render();
                     if (y1 > y2)
                     {
                         while (y1 > y2)
                         {
-                            Program.Frame[y2 += 1][x2] = Program.Home;
-                            Helper.Render();
+                            Frame[y2 += 1][x2] = Home;
+                            Render();
                         }
                     }
                     else if (y1 < y2)
                     {
                         while (y1 > y2)
                         {
-                            Program.Frame[y2 -= 1][x2] = Program.Home;
-                            Helper.Render();
+                            Frame[y2 -= 1][x2] = Home;
+                            Render();
                         }
                     }
                 }
@@ -39,22 +41,22 @@ namespace TreasureIsland
                 while (x1 <= x2)
                 {
                     y2 = defaultY;
-                    Program.Frame[y2][x2 -= 1] = Program.Home;
-                    Helper.Render();
+                    Frame[y2][x2 -= 1] = Home;
+                    Render();
                     if (y1 > y2)
                     {
                         while (y1 > y2)
                         {
-                            Program.Frame[y2 += 1][x2] = Program.Home;
-                            Helper.Render();
+                            Frame[y2 += 1][x2] = Home;
+                            Render();
                         }
                     }
                     else if (y1 < y2)
                     {
                         while (y1 < y2)
                         {
-                            Program.Frame[y2 -= 1][x2] = Program.Home;
-                            Helper.Render();
+                            Frame[y2 -= 1][x2] = Home;
+                            Render();
                         }
                     }
                 }
@@ -64,22 +66,22 @@ namespace TreasureIsland
         public static void RobotOnMap(int x, int y)
         {
             Console.SetCursorPosition(x, y);
-            Console.Write(Program.Robot);
-            Program.Frame[y][x] = Program.Robot;
+            Console.Write(Robot);
+            Frame[y][x] = Robot;
         }
 
         public static void TreasureOnMap(int x, int y)
         {
             Console.SetCursorPosition(x, y);
-            Console.Write(Program.Treasure);
-            Program.Frame[y][x] = Program.Treasure;
+            Console.Write(Treasure);
+            Frame[y][x] = Treasure;
         }
 
         public static void BridgeOnMap(int x, int y)
         {
             Console.SetCursorPosition(x, y);
-            Console.Write(Program.Bridge);
-            Program.Frame[y][x] = Program.Bridge;
+            Console.Write(Bridge);
+            Frame[y][x] = Bridge;
         }
 
         public static void PathOnMap(int x1, int y1, int x2, int y2)
@@ -92,8 +94,8 @@ namespace TreasureIsland
                         while (y1 > y2)
                         {
                             y2 += 1;
-                            Program.Frame[y2][x2] = Program.Path;
-                            Helper.Render();
+                            Frame[y2][x2] = Path;
+                            Render();
                         }
                     }
                     else if (y1 < y2)
@@ -101,13 +103,13 @@ namespace TreasureIsland
                         while (y1 < y2)
                         {
                             y2 -= 1;
-                            Program.Frame[y2][x2] = Program.Path;
-                            Helper.Render();
+                            Frame[y2][x2] = Path;
+                            Render();
                         }
                     }
 
-                    Program.Frame[y2][x2] = Program.Path;
-                    Helper.Render();
+                    Frame[y2][x2] = Path;
+                    Render();
                 }
 
                 if (x1 > x2)
@@ -122,8 +124,8 @@ namespace TreasureIsland
                                 break;
                             }
 
-                            Program.Frame[y2][x2 += 1] = Program.Path;
-                            Helper.Render();
+                            Frame[y2][x2 += 1] = Path;
+                            Render();
                         }
                         else if (y1 < y2)
                         {
@@ -134,8 +136,8 @@ namespace TreasureIsland
                             }
                         }
 
-                        Program.Frame[y2][x2 += 1] = Program.Path;
-                        Helper.Render();
+                        Frame[y2][x2 += 1] = Path;
+                        Render();
                     }
                 }
 
@@ -160,8 +162,8 @@ namespace TreasureIsland
                             }
                         }
 
-                        Program.Frame[y2][x2 -= 1] = Program.Path;
-                        Helper.Render();
+                        Frame[y2][x2 -= 1] = Path;
+                        Render();
                     }
                 }
             }
@@ -177,8 +179,8 @@ namespace TreasureIsland
                         while (y1 > y2)
                         {
                             y2 += 1;
-                            Program.Frame[y2][x2] = Program.River;
-                            Helper.Render();
+                            Frame[y2][x2] = River;
+                            Render();
                         }
                     }
                     else if (y1 < y2)
@@ -186,13 +188,13 @@ namespace TreasureIsland
                         while (y1 < y2)
                         {
                             y2 -= 1;
-                            Program.Frame[y2][x2] = Program.River;
-                            Helper.Render();
+                            Frame[y2][x2] = River;
+                            Render();
                         }
                     }
 
-                    Program.Frame[y2][x2] = Program.River;
-                    Helper.Render();
+                    Frame[y2][x2] = River;
+                    Render();
                 }
 
                 if (x1 > x2)
@@ -207,8 +209,8 @@ namespace TreasureIsland
                                 break;
                             }
 
-                            Program.Frame[y2][x2 += 1] = Program.River;
-                            Helper.Render();
+                            Frame[y2][x2 += 1] = River;
+                            Render();
                         }
                         else if (y1 < y2)
                         {
@@ -219,8 +221,8 @@ namespace TreasureIsland
                             }
                         }
 
-                        Program.Frame[y2][x2 += 1] = Program.River;
-                        Helper.Render();
+                        Frame[y2][x2 += 1] = River;
+                        Render();
                     }
                 }
 
@@ -245,8 +247,8 @@ namespace TreasureIsland
                             }
                         }
 
-                        Program.Frame[y2][x2 -= 1] = Program.River;
-                        Helper.Render();
+                        Frame[y2][x2 -= 1] = River;
+                        Render();
                     }
                 }
             }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using static TreasureIsland.FileReader;
+using static TreasureIsland.ObjectsOnMap;
 
 namespace TreasureIsland
 {
@@ -22,75 +24,77 @@ namespace TreasureIsland
             Console.ForegroundColor = ConsoleColor.White;
             Console.CursorVisible = false;
 
-            FileReader.ReadingFromFile(FilePath);
+            ReadingFromFile(FilePath);
 
             MapInit.FrameMaker();
 
             Helper.Render();
 
+            RobotOnMap(BaseParsing()[0],
+                       BaseParsing()[1]);
 
-            ObjectsOnMap.HomeOnMap(FileReader.BaseParsing()[0],
-                                   FileReader.BaseParsing()[1],
-                                   FileReader.BaseParsing()[2],
-                                   FileReader.BaseParsing()[3]);
-
-            ObjectsOnMap.RobotOnMap(FileReader.BaseParsing()[2],
-                                    FileReader.BaseParsing()[3]);
+            RiverPainter();
 
 
+            TreasureOnMap(TreasureParsing()[0], 
+                          TreasureParsing()[1]);
 
-            ObjectsOnMap.RiverOnMap(FileReader.RiverParsing()[0],
-                                    FileReader.RiverParsing()[1],
+            PathPainter();
 
-                                    FileReader.RiverParsing()[2],
-                                    FileReader.RiverParsing()[3]);
+            HomeOnMap(BaseParsing()[0],
+                      BaseParsing()[1],
+                      BaseParsing()[2],
+                      BaseParsing()[3]);
 
-            ObjectsOnMap.RiverOnMap(FileReader.RiverParsing()[2],
-                                    FileReader.RiverParsing()[3],
-
-                                    FileReader.RiverParsing()[4],
-                                    FileReader.RiverParsing()[5]);
-
-            ObjectsOnMap.RiverOnMap(FileReader.RiverParsing()[4],
-                                    FileReader.RiverParsing()[5],
-
-                                    FileReader.RiverParsing()[6],
-                                    FileReader.RiverParsing()[7]);
-
-            ObjectsOnMap.RiverOnMap(FileReader.RiverParsing()[6],
-                                    FileReader.RiverParsing()[7],
-
-                                    FileReader.RiverParsing()[8],
-                                    FileReader.RiverParsing()[9]);
-
-            ObjectsOnMap.RiverOnMap(FileReader.RiverParsing()[8],
-                                    FileReader.RiverParsing()[9],
-
-                                    FileReader.RiverParsing()[10],
-                                    FileReader.RiverParsing()[11]);
-
-
-            ObjectsOnMap.BridgeOnMap(FileReader.BridgeParsing()[0],
-                                     FileReader.BridgeParsing()[1]);
-
-            ObjectsOnMap.TreasureOnMap(FileReader.TreasureParsing()[0],
-                                       FileReader.TreasureParsing()[1]);
-
-            ObjectsOnMap.PathOnMap(FileReader.BaseParsing()[2], 
-                                   FileReader.BaseParsing()[3], 
-                                   FileReader.BridgeParsing()[0], 
-                                   FileReader.BridgeParsing()[1]);
-
-            ObjectsOnMap.PathOnMap(FileReader.BridgeParsing()[0],
-                                   FileReader.BridgeParsing()[1],
-                                   FileReader.TreasureParsing()[0],
-                                   FileReader.TreasureParsing()[1]);
+            BridgeOnMap(BridgeParsing()[0],
+                        BridgeParsing()[1]);
 
 
             Helper.BackCursor();
 
 
             Console.ReadKey();
+        }
+
+        private static void PathPainter()
+        {
+            PathOnMap(BaseParsing()[0],
+                BaseParsing()[1],
+                BridgeParsing()[0],
+                BridgeParsing()[1]);
+
+            PathOnMap(BridgeParsing()[0],
+                BridgeParsing()[1],
+                TreasureParsing()[0],
+                TreasureParsing()[1]);
+        }
+
+        private static void RiverPainter()
+        {
+            RiverOnMap(RiverParsing()[0],
+                    RiverParsing()[1],
+                    RiverParsing()[2],
+                    RiverParsing()[3]);
+
+            RiverOnMap(RiverParsing()[2],
+                    RiverParsing()[3],
+                    RiverParsing()[4],
+                    RiverParsing()[5]);
+
+            RiverOnMap(RiverParsing()[4],
+                    RiverParsing()[5],
+                    RiverParsing()[6],
+                    RiverParsing()[7]);
+
+            RiverOnMap(RiverParsing()[6],
+                    RiverParsing()[7],
+                    RiverParsing()[8],
+                    RiverParsing()[9]);
+
+            RiverOnMap(RiverParsing()[8],
+                    RiverParsing()[9],
+                    RiverParsing()[10],
+                    RiverParsing()[11]);
         }
     }
 }
