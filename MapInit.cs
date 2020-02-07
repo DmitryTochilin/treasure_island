a786272da1c1d8b9f63f93258d82890c185a4b21
DmitryTochilin <Dmitry.Tochilin@firstlinesoftware.com>
2020-02-07 17:37:12 +0300

    Created MapMaker method

--- MapInit.cs
+++ MapInit.cs
@@ -30,6 +30,65 @@ namespace TreasureIsland
 
         }
 
+        public static void MapMaker()
+        {
+            const int width = 40;
+            const int height = 20;
+
+            var map = new char[width, height];
+            int num = 0;
+
+
+            for (int h = 0; h < map.GetLength(1); h++)
+            {
+
+                for (int w = 0; w < map.GetLength(0); w++)
+                {
+
+                    if (h == height - 1 && w != 0)
+                    {
+                        map[w, h] = '#';
+                        Console.Write(map[w, h]);
+                    }
+                    else if (w == width - 1 && h != 0)
+                    {
+                        map[w, h] = '#';
+                        Console.Write(map[w, h]);
+                    }
+                    else if (w == 0)
+                    {
+                        map[w, h] = Convert.ToChar(num.ToString());
+                        Console.Write(map[w, h]);
+                        num = num + 1;
+                        if (num == 10)
+                        {
+                            num = 0;
+                        }
+                    }
+                    else if (h == 0)
+                    {
+                        map[w, h] = Convert.ToChar(num.ToString());
+                        Console.Write(map[w, h]);
+                        num = num + 1;
+                        if (num == 10)
+                        {
+                            num = 0;
+                        }
+                        if (h == 0 && w == 39)
+                        {
+                            num = 1;
+                        }
+                    }
+                    else
+                    {
+                        map[w, h] = '.';
+                        Console.Write(map[w, h]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
     }
 
 }
