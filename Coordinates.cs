62d2a62503be6a40d643e9f09f298a6ce23b0418
DmitryTochilin <Dmitry.Tochilin@firstlinesoftware.com>
2020-02-21 10:58:56 +0300

    Created MaxX and MaxY methods

--- Coordinates.cs
+++ Coordinates.cs
@@ -4,20 +4,77 @@ namespace TreasureIsland
 {
     class Coordinates
     {
-        public static int TreasureX => TreasureParsing()[0];
+        public static int TreasureX = TreasureParsing()[0];
 
-        public static int TreasureY => TreasureParsing()[1];
+        public static int TreasureY = TreasureParsing()[1];
 
-        public static int BridgeX => BridgeParsing()[0];
+        public static int BridgeX = BridgeParsing()[0];
 
-        public static int BridgeY => BridgeParsing()[1];
+        public static int BridgeY = BridgeParsing()[1];
 
-        public static int BaseX1 => BaseParsing()[0];
+        public static int BaseX1 = BaseParsing()[0];
 
-        public static int BaseY1 => BaseParsing()[1];
+        public static int BaseY1 = BaseParsing()[1];
 
-        public static int BaseX2 => BaseParsing()[2];
+        public static int BaseX2 = BaseParsing()[2];
 
-        public static int BaseY2 => BaseParsing()[3];
+        public static int BaseY2 = BaseParsing()[3];
+
+        public static int RiverX1 = RiverParsing()[0];
+
+        public static int RiverY1 = RiverParsing()[1];
+
+        public static int RiverX2 = RiverParsing()[2];
+
+        public static int RiverY2 = RiverParsing()[3];
+
+        public static int RiverX3 = RiverParsing()[4];
+
+        public static int RiverY3 = RiverParsing()[5];
+
+        public static int RiverX4 = RiverParsing()[6];
+
+        public static int RiverY4 = RiverParsing()[7];
+
+        public static int RiverX5 = RiverParsing()[8];
+
+        public static int RiverY5 = RiverParsing()[9];
+
+        public static int RiverX6 = RiverParsing()[10];
+
+        public static int RiverY6 = RiverParsing()[11];
+
+
+        //this method not used in current realization
+        public static int MaxX()
+        {
+            int[] array = { TreasureX, BridgeX, BaseX1, BaseX2, RiverX1, RiverX2, RiverX3, RiverX4, RiverX5, RiverX6 };
+
+            int max = int.MinValue;
+            foreach (int x in array)
+            {
+                if (x >= max)
+                {
+                    max = x;
+                }
+            }
+            return max;
+        }
+
+        //this method not used in current realization
+        public static int MaxY()
+        {
+            int[] array = { TreasureY, BridgeY, BaseY1, BaseY2, RiverY1, RiverY2, RiverY3, RiverY4, RiverY5, RiverY6 };
+
+            int max = int.MinValue;
+            foreach (int y in array)
+            {
+                if (y >= max)
+                {
+                    max = y;
+                }
+            }
+            return max;
+        }
     }
 }
